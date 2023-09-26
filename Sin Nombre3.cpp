9d67a9d28ad7f545c5797833b4e103283e3d4b28
ShhhD20 <142339741+ShhhD20@users.noreply.github.com>
2023-09-26 12:30:19 -0600

    Update Sin Nombre3.cpp

--- Sin Nombre3.cpp
+++ Sin Nombre3.cpp
@@ -3,12 +3,15 @@
 #include <time.h>
 
 int main (){
-	int matriz1[3][3];
-	int matriz2[3][3];
+	int ans = 3;
+	int operacion = 0;
+	int cres = 0;
 	srand(time(NULL));
 	
-	for(int a = 0; a < 3; a++){
-		for(int b = 0; b < 3; b++){
+	int matriz1[ans][ans], matriz2[ans][ans];
+	int Mfinal[ans][ans];
+	for(int a = 0; a < ans; a++){
+		for(int b = 0; b < ans; b++){
 			matriz1[a][b] = rand() % 6;
 			matriz2[a][b] = rand() % 6;
 			printf("  %d", matriz1[a][b]);
@@ -16,12 +19,18 @@ int main (){
 		printf("\n");
 	}
 	printf("\n\n");
-	for(int c = 0; c < 3; c++){
-		for(int d = 0; d < 3; d++){
+	for(int c = 0; c < ans; c++){
+		for(int d = 0; d < ans; d++){
 			printf("  %d", matriz2[c][d]);
 		}
 		printf("\n");
 	}
+	printf("\n\n");
+	
+	for(int e = 0; e < ans; e++){
+		for(int f = 0; f < ans; f++){
+			operacion = matriz1[e][f] * matriz2[f][e];
+		}
+		Mfinal[e][cres];
 		
-	return 0;
-}
+	}
