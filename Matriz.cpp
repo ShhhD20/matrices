446dbfa2c909103eb79dc7387ef83c6d51d97133
ShhhD20 <142339741+ShhhD20@users.noreply.github.com>
2023-09-25 21:59:08 -0600

    Update Matriz.cpp

--- Matriz.cpp
+++ Matriz.cpp
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main (){
-	int matriz[5][5] = {{0, 0, 7, 1, 0}, {1, 2, 4, 5, 1}, {2, 4, 2, 3, 2}, {3, 6, 0, 4, 3}, {4, 8, 2, 5, 9}};
+	int matriz[5][5];
 	int operador;
 	int b = 0;
+	srand(time(NULL));
 	
 	for(int a = 0; a < 5; a++){
+		matriz[a][0] = rand() % 10;
+		matriz[a][1] = rand() % 10;
+		matriz[a][2] = rand() % 10;
+		matriz[a][3] = rand() % 10;
+		matriz[a][4] = rand() % 10;
+		
 		printf("\n  %d", matriz[a][0]);
 		printf("\t%d", matriz[a][1]);
 		printf("\t%d", matriz[a][2]);
@@ -16,9 +24,7 @@ int main (){
 		operador += matriz[a][b];
 		b++;
 	}
-	
-	printf("\n%d", operador);
-	
+	printf("\nSuma de la diagonal: %d", operador);
 	
 	return 0;
 }
