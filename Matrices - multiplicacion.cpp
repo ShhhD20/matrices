6c98087278877d2768399f920c1c13acd1f491eb
ShhhD20 <142339741+ShhhD20@users.noreply.github.com>
2023-09-28 17:44:44 -0600

    Add files via upload

---
+++ Matrices - multiplicacion.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main (){
+	int ans = 3;
+	int operacion = 0;
+	srand(time(NULL));
+	int matriz1[ans][ans], matriz2[ans][ans];
+	int Mfinal[ans][ans];
+	
+	for(int a = 0; a < ans; a++){
+		for(int b = 0; b < ans; b++){
+			matriz1[a][b] = rand() % 6;
+			matriz2[a][b] = rand() % 6;
+			printf("  %d", matriz1[a][b]);
+		}
+		printf("\n");
+	}
+	printf("\n\n");
+	for(int c = 0; c < ans; c++){
+		for(int d = 0; d < ans; d++){
+			printf("  %d", matriz2[c][d]);
+		}
+		printf("\n");
+	}
+	printf("\n\n");
+	for(int num = 0; num < ans; num++){
+		for(int e = 0; e < ans; e++){
+			for(int f = 0; f < ans; f++){
+				operacion += matriz1[e][f] * matriz2[f][e];
+			}
+			Mfinal[e][num] = operacion;
+		}
+	}
+	for(int g = 0; g < ans; g++){
+		for(int h = 0; h < ans; h++){
+			printf("  %d", Mfinal[g][h]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
